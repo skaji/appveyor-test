91062c3bc2b05ef6856dd5bf31f1f659792f5b1a
Shoichi Kaji <skaji@cpan.org>
2018-04-19 17:04:44 +0200

    hoge

--- test.pl
+++ test.pl
@@ -1,72 +1,38 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use HTTP::Tiny;
+use Path::Tiny;
 
-use POSIX 'strftime';
-use File::Copy 'copy';
-use File::Copy::Recursive 'rcopy';
-use File::Find 'find';
-use File::Temp 'tempdir';
-use File::Path 'remove_tree';
+my $res = HTTP::Tiny->new->mirror(
+    "https://cpan.metacpan.org/authors/id/M/MS/MSTROUT/App-FatPacker-0.010007.tar.gz",
+    "App-FatPacker-0.010007.tar.gz",
+);
+warn "$res->{status} $res->{reason}";
 
-sub show_mtimes {
-    my @file;
-    for my $e (@_) {
-        if (-f $e) {
-            push @file, $e;
-        } elsif (-d $e) {
-            find({wanted => sub { push @file, $_ if -f }, no_chdir => 1}, $e);
-        }
-    }
-    map {
-        my $file = $_;
-        my @stat = stat $file;
-        warn sprintf "atime %s, mtime %s, ctime %s %s\n",
-            strftime("%H:%M:%S", localtime $stat[8]),
-            strftime("%H:%M:%S", localtime $stat[9]),
-            strftime("%H:%M:%S", localtime $stat[10]),
-            $file;
-    } sort @file;
-}
-sub touch_now {
-    my @file;
-    for my $e (@_) {
-        if (-f $e) {
-            push @file, $e;
-        } elsif (-d $e) {
-            find({wanted => sub { push @file, $_ if -f }, no_chdir => 1}, $e);
-        }
-    }
-    utime $^T, $^T, @file;
-}
-sub info {
-    warn "--> @_\n";
-}
+system "tar", "xf", "App-FatPacker-0.010007.tar.gz";
+warn $?;
 
-remove_tree "share_copy" if -d "share_copy";
-unlink "file_copy.txt" if -f "file_copy.txt";
+chdir "App-FatPacker-0.010007" or die;
 
-info "START \$^T =", strftime("%H:%M:%S", localtime $^T), "($^T)";
+path("hoge.patch")->spew(<<'___');
+diff --git Makefile.PL Makefile.PL
+index 1077f48..8a8c7d9 100644
+--- Makefile.PL
++++ Makefile.PL
+@@ -6,6 +6,8 @@ use 5.008000;
+ 
+ (do 'maint/Makefile.PL.include' or die $@) unless -f 'META.yml';
+ 
++warn "HOOKED";
++
+ WriteMakefile(
+   NAME => 'App::FatPacker',
+   VERSION_FROM => 'lib/App/FatPacker.pm',
+___
 
-for (1..2) {
-    info "====== $_ time ======";
-    info "1. first";
-    show_mtimes "share";
-    info "2. sleep 2";
-    sleep 2;
-    info "3. copy share";
-    rcopy "share", "share_copy" or die $!;
-    info "3-1. touch now";
-    touch_now "share_copy";
-    show_mtimes "share", "share_copy";
+system "patch", "--batch", "-p0", "-i", "hoge.patch";
 
-    info "4. sleep 2";
-    sleep 2;
-    info "5. copy share/file.txt";
-    copy "share/file.txt", "file_copy.txt" or die $!;
-    info "5-1. touch now";
-    touch_now "file_copy.txt";
-    show_mtimes "share/file.txt", "file_copy.txt";
-}
+warn $?;
 
-
+warn path("Makefile.PL")->slurp;
